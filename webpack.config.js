953d439c2f9fe3a6ca6f7e00de8fddaec7eb0b26
vm-raccoon <vm.raccoon@gmail.com>
2021-03-04 22:20:11 +0200

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
+
+module.exports = {
+    mode: "production",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        port: 1919,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, "src/index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+    ],
+    resolve: {
+        extensions: [".js"],
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }, {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ],
+    },
+}
